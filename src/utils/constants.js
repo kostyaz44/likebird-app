570bd352d4728d9ebad40da5233eea6fd53d5897
kostyaz44 <kostya09876@yandex.ru>
2026-03-13 23:41:14 +0300

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-// ===== ВЕРСИЯ ПРИЛОЖЕНИЯ =====
-export const APP_VERSION = '3.0';
-export const DATA_VERSION = 2; // Increment when data structure changes
+export const APP_VERSION = '3.1';
+export const DATA_VERSION = 2;
+ 
